ce208c57bc11cd46c999d841208d3435151a2462
David Andersson <david.andersson@zocom.se>
2023-11-15 10:14:41 +0100

    9.3

--- script.js
+++ script.js
@@ -19,3 +19,38 @@ userInput.addEventListener('input', event => {
 	console.log('input event', event);
 	output.innerText = 'user-input input: ' + event.target.value
 })
+
+
+// 3a Gör en button som gör ett annat element osynligt, med hjälp av CSS-klasser.Gör en annan button som gör elementet synligt.
+
+// 3b Gör en tredje button som "togglar" elementet. Alltså varannan gång gör elementet synligt och varannan gång gör det osynligt. Men använd en if-sats i stället för classList.toggle.
+const showBtn = document.querySelector('#show-btn')
+const hideBtn = document.querySelector('#hide-btn')
+const toggleBtn = document.querySelector('#toggle-btn')
+const showHide = document.querySelector('#show-hide')
+
+let isVisible = false
+
+showBtn.addEventListener('click', () => {
+	showHide.classList.remove('hidden')
+})
+
+hideBtn.addEventListener('click', () => {
+	showHide.classList.add('hidden')
+})
+
+// Använd console.log för att kontrollera vad variabler har för värde:
+// console.log('toggle button?', toggleBtn);
+toggleBtn.addEventListener('click', () => {
+	// classList.toggle är en enklare lösning
+	// Felsök med console.log:
+	// console.log(1);
+	if( isVisible ) {
+		// console.log(2);
+		showHide.classList.add('hidden')
+	} else {
+		// console.log(3);
+		showHide.classList.remove('hidden')
+	}
+	isVisible = !isVisible
+})
